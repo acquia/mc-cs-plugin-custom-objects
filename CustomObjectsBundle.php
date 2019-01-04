4c76bf178df2033b61fe8822ec4b8ac485d9320c
John Linhart <admin@escope.cz>
2019-01-04 13:15:42 +0100

    Add prefix index to the text value column

--- CustomObjectsBundle.php
+++ CustomObjectsBundle.php
@@ -13,8 +13,87 @@ declare(strict_types=1);
 
 namespace MauticPlugin\CustomObjectsBundle;
 
+use Doctrine\DBAL\Schema\Schema;
 use Mautic\PluginBundle\Bundle\PluginBundleBase;
+use Mautic\PluginBundle\Entity\Plugin;
+use Mautic\CoreBundle\Factory\MauticFactory;
+use Doctrine\DBAL\Connection;
 
 class CustomObjectsBundle extends PluginBundleBase
 {
+    /**
+     * @param Plugin        $plugin
+     * @param MauticFactory $factory
+     * @param array|null    $metadata
+     * @param Schema|null   $installedSchema
+     *
+     * @throws \Exception
+     */
+    public static function onPluginInstall(Plugin $plugin, MauticFactory $factory, $metadata = null, $installedSchema = null)
+    {
+        parent::onPluginInstall($plugin, $factory, $metadata, $installedSchema);
+        $queries = [self::createIndexQueryIfDoesNotExist($installedSchema)];
+        self::commit($factory->getDatabase(), $queries);
+    }
+
+    /**
+    * @param Plugin        $plugin
+    * @param MauticFactory $factory
+    * @param array|null    $metadata
+    * @param Schema|null   $installedSchema
+    *
+    * @throws \Exception
+    */
+    static public function onPluginUpdate(Plugin $plugin, MauticFactory $factory, $metadata = null, Schema $installedSchema = null)
+    {
+        $queries = [self::createIndexQueryIfDoesNotExist($installedSchema)];
+        self::commit($factory->getDatabase(), $queries);
+    }
+
+    /**
+     * @param Schema $schema
+     * 
+     * @return string
+     */
+    static private function createIndexQueryIfDoesNotExist(Schema $schema): string
+    {
+        $tableName = MAUTIC_TABLE_PREFIX.'custom_field_value_text';
+        $indexName = MAUTIC_TABLE_PREFIX.'value_index';
+
+        if (!$schema->hasTable($tableName)) {
+            return '';
+        }
+        
+        if ($schema->getTable($tableName)->hasIndex($indexName)) {
+            return '';
+        }
+        
+        return "CREATE INDEX {$indexName} ON {$tableName} (value(64))";
+    }
+
+    /**
+     * @param Connection $connection
+     * @param array $queries
+     */
+    static private function commit(Connection $connection, array $queries): void
+    {
+        if (!empty($queries)) {
+
+            $connection->beginTransaction();
+            try {
+                foreach ($queries as $query) {
+                    if (!$query) {
+                        continue;
+                    }
+                    $connection->query($query);
+                }
+
+                $connection->commit();
+            } catch (\Exception $e) {
+                $connection->rollback();
+
+                throw $e;
+            }
+        }
+    }
 }
