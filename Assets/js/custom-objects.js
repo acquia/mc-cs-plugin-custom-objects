7b8a2a2c2478ee434bff6c5ddc204de19691b50c
John Linhart <admin@escope.cz>
2019-01-11 13:03:58 +0100

    Load assets globally

---
+++ Assets/js/custom-objects.js
@@ -0,0 +1,34 @@
+mQuery(function() {
+    CustomObjects.handleTabOnShow();
+});
+
+CustomObjects = {
+
+    handleTabOnShow: function() {
+        mQuery('a.custom-object-tab[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+            let customObjectId = mQuery(e.target).attr('data-custom-object-id');
+            let contactId = mQuery('input#leadId').val();
+            CustomObjects.getItemsForObject(customObjectId, contactId, function(response) {
+                CustomObjects.refreshTabContent(customObjectId, response.newContent);
+            });
+        });
+    },
+
+    getItemsForObject: function(customObjectId, contactId, callback) {
+        mQuery.ajax({
+            type: 'GET',
+            url: mauticBaseUrl+'s/custom/object/'+customObjectId+'/item',
+            data: {contactId: contactId},
+            success: function (response) {
+                callback(response);
+            },
+        });
+    },
+
+    refreshTabContent: function(customObjectId, content) {
+        let tabContent = mQuery('#custom-object-'+customObjectId+'-container');
+        tabContent.html(content);
+        console.log(tabContent, content);
+    },
+
+}
---
+++ EventListener/AssetsSubscriber.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * @copyright   2019 Mautic, Inc. All rights reserved
+ * @author      Mautic, Inc.
+ *
+ * @link        https://mautic.com
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace MauticPlugin\CustomObjectsBundle\EventListener;
+
+use Mautic\CoreBundle\CoreEvents;
+use Mautic\CoreBundle\EventListener\CommonSubscriber;
+use Mautic\CoreBundle\Event\CustomTemplateEvent;
+use Mautic\CoreBundle\Templating\Helper\AssetsHelper;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+class AssetsSubscriber extends CommonSubscriber
+{
+    /**
+     * @var AssetsHelper
+     */
+    private $assetHelper;
+
+    /**
+     *
+     * @param AssetsHelper $assetHelper
+     */
+    public function __construct(AssetsHelper $assetHelper)
+    {
+        $this->assetHelper = $assetHelper;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::REQUEST => ['loadAssets', -255],
+        ];
+    }
+
+    /**
+     * @param GetResponseEvent $event
+     */
+    public function loadAssets(GetResponseEvent $event)
+    {
+        if ($event->isMasterRequest()) {
+            $this->assetHelper->addScript('plugins/CustomObjectsBundle/Assets/js/custom-objects.js');
+        }
+    }
+}
