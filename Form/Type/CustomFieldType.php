893ecf5b978427cee45abb2da95b26f7c0aa25bc
Lukas Drahy <lukas@drahy.net>
2019-02-01 16:24:59 +0100

    Remove index from CF form as unused

--- Form/Type/CustomFieldType.php
+++ Form/Type/CustomFieldType.php
@@ -95,8 +95,6 @@ class CustomFieldType extends AbstractType
             HiddenType::class
         );
 
-        $builder->add('index',HiddenType::class,['mapped' => false]);
-
         if ($customObjectForm) {
             // Possibility to mark field as deleted in POST data
             $builder->add('deleted',HiddenType::class,['mapped' => false]);
