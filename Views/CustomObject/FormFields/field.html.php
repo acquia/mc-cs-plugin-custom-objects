3259ca6008f157b48f8d3a633278d7e53d60ec2b
Lukas Drahy <lukas@drahy.net>
2019-02-08 12:45:18 +0100

    Add Mautic standard docblocks

--- Views/CustomObject/FormFields/field.html.php
+++ Views/CustomObject/FormFields/field.html.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+ * @copyright   2018 Mautic, Inc. All rights reserved
+ * @author      Mautic, Inc.
+ *
+ * @link        https://mautic.com
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
 $customFieldEntity = $customField->vars['data'];
 $customField->vars['index'] = $customField->vars['name'];
 ?>
