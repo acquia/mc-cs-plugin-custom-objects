6ff21a16135460dd1d192778ce9e91c3738f7c11
Lukas Drahy <lukas@drahy.net>
2019-02-13 16:17:35 +0100

    Add DateTime cf type

---
+++ CustomFieldType/DateTimeType.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * @copyright   2019 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace MauticPlugin\CustomObjectsBundle\CustomFieldType;
+
+use MauticPlugin\CustomObjectsBundle\Entity\CustomField;
+use MauticPlugin\CustomObjectsBundle\Entity\CustomFieldValueDateTime;
+use MauticPlugin\CustomObjectsBundle\Entity\CustomFieldValueInterface;
+use MauticPlugin\CustomObjectsBundle\Entity\CustomItem;
+
+class DateTimeType extends AbstractCustomFieldType
+{
+    protected $key = 'datetime';
+
+    /**
+     * @return string
+     */
+    public function getSymfonyFormFiledType(): string
+    {
+        return \Symfony\Component\Form\Extension\Core\Type\DateTimeType::class;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEntityClass(): string
+    {
+        return CustomFieldValueDateTime::class;
+    }
+
+    /**
+     * @param CustomField $customField
+     * @param CustomItem  $customItem
+     * @param mixed|null  $value
+     *
+     * @return CustomFieldValueInterface
+     */
+    public function createValueEntity(CustomField $customField, CustomItem $customItem, $value = null): CustomFieldValueInterface
+    {
+        return new CustomFieldValueDateTime($customField, $customItem, new \DateTime($value));
+    }
+
+    /**
+     * @return string
+     */
+    public function getTableName(): string
+    {
+        return MAUTIC_TABLE_PREFIX.'custom_field_value_datetime';
+    }
+
+    /**
+     * @return string
+     */
+    public function getTableAlias(): string
+    {
+        return 'cfvdatetime';
+    }
+
+    /**
+     * @return array
+     */
+    public function getOperators(): array
+    {
+        $allOperators = parent::getOperators();
+        $allowedOperators = array_flip(['=', '!=', 'gt', 'gte', 'lt', 'lte', 'empty', '!empty', 'between', '!between', 'in', '!in']);
+
+        return array_intersect_key($allOperators, $allowedOperators);
+    }
+}
