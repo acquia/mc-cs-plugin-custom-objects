1f55229a6e9a627fd7c1fe3d6da8754d6b42b5dd
John Linhart <jan@linhart.email>
2019-03-05 15:53:16 +0100

    Update Helper/QueryFilterHelper.php
    Co-Authored-By: galvani <galvani78@gmail.com>

--- Helper/QueryFilterHelper.php
+++ Helper/QueryFilterHelper.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
  * @author      Mautic, Inc.
  *
  * @link        http://mautic.com
- * @created     8.2.19
+ *
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
