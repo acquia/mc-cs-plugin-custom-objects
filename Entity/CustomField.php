521e22f02b321c7e9941da753c3a6f86e86132d4
Lukas Drahy <lukas@drahy.net>
2019-03-11 13:22:25 +0100

    Disable length validator for default value

--- Entity/CustomField.php
+++ Entity/CustomField.php
@@ -143,7 +143,7 @@ class CustomField extends FormEntity implements UniqueEntityInterface
         $metadata->addPropertyConstraint('type', new Assert\Length(['max' => 255]));
         $metadata->addPropertyConstraint('customObject', new Assert\NotBlank());
         // @todo this breaks usage of DAteTime type
-        $metadata->addPropertyConstraint('defaultValue', new Assert\Length(['max' => 255]));
+//        $metadata->addPropertyConstraint('defaultValue', new Assert\Length(['max' => 255]));
     }
 
     /**
