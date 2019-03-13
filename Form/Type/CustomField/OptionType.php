fe279684ad7a8f42457e1e2ddaae933391872c62
Lukas Drahy <lukas@drahy.net>
2019-03-13 12:43:08 +0100

    CF Option form prototypes

---
+++ Form/Type/CustomField/OptionType.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+* @copyright   2019 Mautic, Inc. All rights reserved
+* @author      Mautic, Inc.
+*
+* @link        https://mautic.com
+*
+* @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+namespace MauticPlugin\CustomObjectsBundle\Form\Type\CustomField;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OptionType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add(
+            'name',
+            TextType::class,
+            [
+                'required' => true,
+            ]
+        );
+
+        $builder->add(
+            'value',
+            TextType::class,
+            [
+                'required' => true,
+            ]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+//                'data_class'         => Params::class,
+                'csrf_protection'    => false,
+            ]
+        );
+    }
+}
--- Form/Type/CustomField/ParamsType.php
+++ Form/Type/CustomField/ParamsType.php
@@ -15,6 +15,7 @@ namespace MauticPlugin\CustomObjectsBundle\Form\Type\CustomField;
 
 use MauticPlugin\CustomObjectsBundle\Entity\CustomField\Params;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
@@ -32,6 +33,19 @@ class ParamsType extends AbstractType
             [
                 'label'      => 'custom.field.label.required_validation_message',
                 'required'   => false,
+            ]
+        );
+
+        $builder->add(
+            'options',
+            CollectionType::class,
+            [
+                'mapped' => false,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'delete_empty' => true,
+                'entry_type' => OptionsType::class,
+                'prototype' => true,
             ]
         );
     }
