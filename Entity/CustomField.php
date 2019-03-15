a8b0a840a2d19d759244eeeecd6a333e7d2cfd62
John Linhart <admin@escope.cz>
2019-03-15 17:40:51 +0100

    This was overwriting the actual value

--- Entity/CustomField.php
+++ Entity/CustomField.php
@@ -253,7 +253,7 @@ class CustomField extends FormEntity implements UniqueEntityInterface
         $fieldOptions     = [
             'label'      => $this->getLabel(),
             'required'   => $this->isRequired(),
-            'data'       => $this->getDefaultValue(),
+            'empty_data' => $this->getDefaultValue(),
             'label_attr' => ['class' => 'control-label'],
             'attr'       => ['class' => 'form-control'],
         ];
