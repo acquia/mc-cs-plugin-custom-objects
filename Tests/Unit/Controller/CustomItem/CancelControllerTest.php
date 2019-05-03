b177faf19d9fc48435d07151e80a93e8d822d94c
Lukas Drahy <lukas@drahy.net>
2019-05-03 11:06:48 +0200

    Add entity unlocking for item cancel action

--- Tests/Unit/Controller/CustomItem/CancelControllerTest.php
+++ Tests/Unit/Controller/CustomItem/CancelControllerTest.php
@@ -13,6 +13,9 @@ declare(strict_types=1);
 
 namespace MauticPlugin\CustomObjectsBundle\Tests\Unit\Controller\CustomItem;
 
+use MauticPlugin\CustomObjectsBundle\Entity\CustomItem;
+use MauticPlugin\CustomObjectsBundle\Entity\CustomObject;
+use MauticPlugin\CustomObjectsBundle\Model\CustomItemModel;
 use MauticPlugin\CustomObjectsBundle\Provider\CustomItemRouteProvider;
 use MauticPlugin\CustomObjectsBundle\Tests\Unit\Controller\ControllerTestCase;
 use MauticPlugin\CustomObjectsBundle\Controller\CustomItem\CancelController;
@@ -24,6 +27,7 @@ class CancelControllerTest extends ControllerTestCase
 
     private $sessionProvider;
     private $routeProvider;
+    private $customItemModel;
 
     /**
      * @var CancelController
@@ -36,9 +40,12 @@ class CancelControllerTest extends ControllerTestCase
 
         $this->sessionProvider  = $this->createMock(CustomItemSessionProvider::class);
         $this->routeProvider    = $this->createMock(CustomItemRouteProvider::class);
+        $this->customItemModel = $this->createMock(CustomItemModel::class);
+
         $this->cancelController = new CancelController(
             $this->sessionProvider,
-            $this->routeProvider
+            $this->routeProvider,
+            $this->customItemModel
         );
 
         $this->addSymfonyDependencies($this->cancelController);
@@ -46,15 +53,49 @@ class CancelControllerTest extends ControllerTestCase
 
     public function testCancelAction(): void
     {
+        $pageNumber = 4;
+
         $this->sessionProvider->expects($this->once())
             ->method('getPage')
-            ->willReturn(4);
+            ->willReturn($pageNumber);
+
+        $this->customItemModel->expects($this->once())
+            ->method('getEntity')
+            ->with(null)
+            ->willReturn(null);
 
         $this->routeProvider->expects($this->once())
             ->method('buildListRoute')
-            ->with(self::OBJECT_ID, 4)
+            ->with(self::OBJECT_ID, $pageNumber)
             ->willReturn('some/route');
 
         $this->cancelController->cancelAction(self::OBJECT_ID);
+    }
+
+    public function testCancelActionWithEntityUnlock(): void
+    {
+        $pageNumber     = 2;
+        $customItemId   = 4;
+        $customItem     = new CustomItem(new CustomObject());
+
+        $this->sessionProvider->expects($this->once())
+            ->method('getPage')
+            ->willReturn($pageNumber);
+
+        $this->customItemModel->expects($this->once())
+            ->method('getEntity')
+            ->with($customItemId)
+            ->willReturn($customItem);
+
+        $this->customItemModel->expects($this->once())
+            ->method('unlockEntity')
+            ->with($customItem);
+
+        $this->routeProvider->expects($this->once())
+            ->method('buildListRoute')
+            ->with(self::OBJECT_ID, $pageNumber)
+            ->willReturn('some/route');
+
+        $this->cancelController->cancelAction(self::OBJECT_ID, $customItemId);
     }
 }
