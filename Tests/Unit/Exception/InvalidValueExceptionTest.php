ba452ee6e2c9b315b1b25d9b33bb7dc6aa0eeda0
John Linhart <admin@escope.cz>
2019-05-30 15:21:33 +0200

    Namespace fix

--- Tests/Unit/Exception/InvalidValueExceptionTest.php
+++ Tests/Unit/Exception/InvalidValueExceptionTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-namespace MauticPlugin\CustomObjectsBundle\Tests\Unit\DTO;
+namespace MauticPlugin\CustomObjectsBundle\Tests\Unit\Exception;
 
 use MauticPlugin\CustomObjectsBundle\Entity\CustomField;
 use MauticPlugin\CustomObjectsBundle\Exception\InvalidValueException;
