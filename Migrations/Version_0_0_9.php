aac97ca5f2533e2a8181f4d41cf03280c859fee8
Ondrej Sibl <ondrej@sibl.info>
2019-06-04 16:26:17 +0200

    Bring aliases back, they will be very useful for API

--- Migrations/Version_0_0_9.php
+++ Migrations/Version_0_0_9.php
@@ -46,8 +46,10 @@ class Version_0_0_9 extends AbstractMigration
      */
     protected function up(): void
     {
-        $this->addSql("ALTER TABLE {$this->concatPrefix($this->tableCustomObject)} ADD alias VARCHAR(255) NOT NULL, ADD INDEX (alias)");
+        $tableCustomObject = $this->concatPrefix($this->tableCustomObject)
+        $this->addSql("ALTER TABLE {$tableCustomObject} ADD alias VARCHAR(255) NOT NULL, ADD INDEX (alias)");
 
-        $this->addSql("ALTER TABLE {$this->concatPrefix($this->tableCustomField)} ADD alias VARCHAR(255) NOT NULL, ADD INDEX (alias)");
+        $tableCustomField = $this->concatPrefix($this->tableCustomField);
+        $this->addSql("ALTER TABLE {$tableCustomField} ADD alias VARCHAR(255) NOT NULL, ADD INDEX (alias)");
     }
 }
