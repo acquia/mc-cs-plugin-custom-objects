202c278a962263863656c1a3aad47816053133f2
John Linhart <admin@escope.cz>
2019-06-14 16:40:03 +0200

    Now we can use the exting methods to validate also the default field values

--- Entity/CustomField.php
+++ Entity/CustomField.php
@@ -27,6 +27,7 @@ use MauticPlugin\CustomObjectsBundle\Repository\CustomFieldRepository;
 use MauticPlugin\CustomObjectsBundle\CustomFieldType\CustomFieldTypeInterface;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use MauticPlugin\CustomObjectsBundle\CustomFieldType\AbstractMultivalueType;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class CustomField extends FormEntity implements UniqueEntityInterface
 {
@@ -175,6 +176,23 @@ class CustomField extends FormEntity implements UniqueEntityInterface
         $metadata->addPropertyConstraint('type', new Assert\Length(['max' => 255]));
         $metadata->addPropertyConstraint('customObject', new Assert\NotBlank());
         $metadata->addPropertyConstraint('defaultValue', new Assert\Length(['max' => 255]));
+        $metadata->addConstraint(new Assert\Callback('validateValue'));
+    }
+
+    /**
+     * Allow different field types to validate the value.
+     *
+     * @param ExecutionContextInterface $context
+     */
+    public function validateValue(ExecutionContextInterface $context): void
+    {
+        try {
+            $this->getTypeObject()->validateValue($this, $this->defaultValue, $context);
+        } catch (\UnexpectedValueException $e) {
+            $context->buildViolation($e->getMessage())
+                // ->atPath('defaultValue') // The default value field disappears after submit.
+                ->addViolation();
+        }
     }
 
     /**
