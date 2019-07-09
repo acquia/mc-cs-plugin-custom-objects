4d3a27ed8365da1b37f3646eb48d1c0fbf41ef65
John Linhart <admin@escope.cz>
2019-07-09 09:59:57 +0200

    CS fix

--- Tests/Unit/EventListener/DynamicContentSubscriberTest.php
+++ Tests/Unit/EventListener/DynamicContentSubscriberTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace MauticPlugin\CustomObjectsBundle\Tests\Unit\EventListener;
 
 use Doctrine\DBAL\Statement;
-use Doctrine\ORM\EntityManager;
 use Mautic\DynamicContentBundle\Event\ContactFiltersEvaluateEvent;
 use Mautic\LeadBundle\Entity\Lead;
 use Mautic\LeadBundle\Segment\Query\QueryBuilder;
