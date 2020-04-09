f72f0477110727a13b9ef1067e6cea66b2e05ae6
Lukas Sykora <lukassykora@seznam.cz>
2020-04-09 15:49:24 +0200

    MAUT-996 / Start API programming

---
+++ Controller/Api/CustomFieldApiController.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace MauticPlugin\CustomObjectsBundle\Controller\Api;
+
+
+use Mautic\CoreBundle\Controller\CommonController;
+use MauticPlugin\CustomObjectsBundle\Model\CustomFieldModel;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class CustomFieldApiController  extends CommonController
+{
+    /**
+     * @var CustomFieldModel
+     */
+    private $customFieldModel;
+
+    public function __construct(
+        CustomFieldModel $customFieldModel
+    ) {
+        $this->customFieldModel = $customFieldModel;
+    }
+    public function pluginGetFieldsAction(Request $request): JsonResponse
+    {
+        $limit = (int) $request->query->get('limit', 20);
+        $offset = (int) $request->query->get('limit', 0);
+        $args = ['limit' => $limit, 'offset' => $offset];
+        $customFields = $this->customFieldModel->fetchEntities($args);
+        return $this->json($customFields);
+    }
+
+}
