6a8dde7e7a59119f7339ba7c2a6ee8eea75a6c78
Lukas Drahy <lukas@drahy.net>
2020-05-11 15:37:08 +0200

    Rewrite test to behave as unit

--- Tests/Unit/EventListener/CustomObjectListFormatSubscriberTest.php
+++ Tests/Unit/EventListener/CustomObjectListFormatSubscriberTest.php
@@ -4,58 +4,98 @@ declare(strict_types=1);
 
 namespace MauticPlugin\CustomObjectsBundle\Tests\Unit\EventListener;
 
+use MauticPlugin\CustomObjectsBundle\CustomObjectEvents;
 use MauticPlugin\CustomObjectsBundle\Event\CustomObjectListFormatEvent;
 use MauticPlugin\CustomObjectsBundle\EventListener\CustomObjectListFormatSubscriber;
 use MauticPlugin\CustomObjectsBundle\Helper\TokenFormatter;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class CustomObjectListFormatSubscriberTest extends TestCase
 {
     /**
+     * @var TokenFormatter|MockObject
+     */
+    private $tokenFormatterMock;
+
+    /**
      * @var CustomObjectListFormatSubscriber
      */
     private $subscriber;
 
+    /**
+     * @var CustomObjectListFormatEvent|MockObject
+     */
+    private $eventMock;
+
     public function setUp(): void
     {
+        $this->tokenFormatterMock = $this->createMock(TokenFormatter::class);
+        $this->subscriber = new CustomObjectListFormatSubscriber($this->tokenFormatterMock);
+        $this->eventMock = $this->createMock(CustomObjectListFormatEvent::class);
+
         parent::setUp();
-
-        $this->subscriber = new CustomObjectListFormatSubscriber(new TokenFormatter());
     }
 
-    public function testInvalidFormat(): void
+    public function testGetSubscribedEvents()
     {
-        $event = new CustomObjectListFormatEvent(['value1'], 'A BAD FORMAT');
-        $this->subscriber->onFormatList($event);
-
-        $this->assertFalse($event->hasBeenFormatted());
-        $this->assertEquals('', $event->getFormattedString());
+        $this->assertSame(
+            [CustomObjectEvents::ON_CUSTOM_OBJECT_LIST_FORMAT => ['onFormatList', 0]],
+            CustomObjectListFormatSubscriber::getSubscribedEvents()
+        );
     }
 
-    public function testNoValues(): void
+    public function testOnFormatInvalid()
     {
-        $event = new CustomObjectListFormatEvent([], 'default');
-        $this->subscriber->onFormatList($event);
+        $format = 'format';
 
-        $this->assertFalse($event->hasBeenFormatted());
+        $this->eventMock->expects($this->once())
+            ->method('getFormat')
+            ->willReturn($format);
+
+        $this->tokenFormatterMock->expects($this->once())
+            ->method('isValidFormat')
+            ->with($format)
+            ->willReturn(false);
+
+        $this->eventMock->expects($this->never())
+            ->method('getCustomObjectValues');
+        $this->tokenFormatterMock->expects($this->never())
+            ->method('format');
+        $this->eventMock->expects($this->never())
+            ->method('setFormattedString');
+
+        $this->subscriber->onFormatList($this->eventMock);
     }
 
-    public function testFormatValues(): void
+    public function testOnFormat()
     {
-        $values = ['value1', 'value2', 'value3'];
+        $format = 'format';
+        $values = ['values'];
+        $formattedString = 'formattedString';
 
-        // Test default formatter
-        $event1 = new CustomObjectListFormatEvent($values, 'default');
-        $this->subscriber->onFormatList($event1);
-        $expected = "value1, value2, value3";
-        $this->assertTrue($event1->hasBeenFormatted());
-        $this->assertEquals($expected, $event1->getFormattedString());
+        $this->eventMock->expects($this->once())
+            ->method('getFormat')
+            ->willReturn($format);
 
-        // Test and formatter
-        $event2 = new CustomObjectListFormatEvent($values, 'and-list');
-        $this->subscriber->onFormatList($event2);
-        $expected = "value1, value2 and value3";
-        $this->assertTrue($event2->hasBeenFormatted());
-        $this->assertEquals($expected, $event2->getFormattedString());
+        $this->tokenFormatterMock->expects($this->once())
+            ->method('isValidFormat')
+            ->with($format)
+            ->willReturn(true);
+
+        $this->eventMock->expects($this->once())
+            ->method('getCustomObjectValues')
+            ->willReturn($values);
+
+        $this->tokenFormatterMock->expects($this->once())
+            ->method('format')
+            ->with($values, $format)
+            ->willReturn($formattedString);
+
+        $this->eventMock->expects($this->once())
+            ->method('setFormattedString')
+            ->with($formattedString);
+
+        $this->subscriber->onFormatList($this->eventMock);
     }
 }
