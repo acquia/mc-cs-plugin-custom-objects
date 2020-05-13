66a714cfc13b89c625749991969df72c7a080b42
Lukas Drahy <lukas@drahy.net>
2020-05-13 10:37:19 +0200

    Add CustomObjectTypeTest::testBuildForm()

--- Tests/Unit/Form/Type/CustomObjectTypeTest.php
+++ Tests/Unit/Form/Type/CustomObjectTypeTest.php
@@ -14,11 +14,22 @@ declare(strict_types=1);
 namespace MauticPlugin\CustomObjectsBundle\Tests\Unit\Form\Type;
 
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\QueryBuilder;
+use Mautic\CategoryBundle\Form\Type\CategoryListType;
+use Mautic\CoreBundle\Form\Type\FormButtonsType;
+use Mautic\CoreBundle\Form\Type\YesNoButtonGroupType;
 use MauticPlugin\CustomObjectsBundle\Entity\CustomObject;
+use MauticPlugin\CustomObjectsBundle\Form\Type\CustomFieldType;
 use MauticPlugin\CustomObjectsBundle\Form\Type\CustomObjectType;
 use MauticPlugin\CustomObjectsBundle\Provider\CustomFieldTypeProvider;
 use MauticPlugin\CustomObjectsBundle\Repository\CustomObjectRepository;
 use PHPUnit\Framework\TestCase;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CustomObjectTypeTest extends TestCase
@@ -56,9 +67,14 @@ class CustomObjectTypeTest extends TestCase
         );
     }
 
-    public function testBuildForm()
+    public function testBuildFormNewObject()
     {
+        $this->buildForm(true, 'nameSingular', 'alias', 'action');
+    }
 
+    public function testBuildFormExistingObject()
+    {
+        $this->buildForm(false, 'nameSingular1', 'alias1', 'action1');
     }
 
     public function testConfigureOptions()
@@ -75,5 +91,182 @@ class CustomObjectTypeTest extends TestCase
             ->with($options);
 
         $this->type->configureOptions($resolver);
+    }
+
+    private function buildForm(bool $isNewObject, string $coNameSingular, string $coAlias, string $action)
+    {
+        $customObject = new CustomObject();
+        $customObject->setNew($isNewObject);
+        $customObject->setNameSingular($coNameSingular);
+        $customObject->setAlias($coAlias);
+
+        $options = [
+            'data'   => $customObject,
+            'action' => $action,
+        ];
+
+        $queryBuilder = $this->createMock(QueryBuilder::class);
+
+        $this->customObjectRepository->expects($this->once())
+            ->method('getMasterObjectQueryBuilder')
+            ->with($customObject)
+            ->willReturn($queryBuilder);
+
+        $builder = $this->createMock(FormBuilderInterface::class);
+        $builder
+            ->method('add')
+            ->withConsecutive(
+                [
+                    'alias',
+                    TextType::class,
+                    [
+                        'label'      => 'custom.object.alias.label',
+                        'required'   => false,
+                        'label_attr' => ['class' => 'control-label'],
+                        'attr'       => [
+                            'class'    => 'form-control',
+                            'tooltip'  => 'custom.field.help.alias',
+                            'readOnly' => !$isNewObject,
+                        ],
+                    ],
+                ],
+                [
+                    'nameSingular',
+                    TextType::class,
+                    [
+                        'label'      => 'custom.object.name.singular.label',
+                        'required'   => true,
+                        'label_attr' => ['class' => 'control-label'],
+                        'attr'       => [
+                            'class'   => 'form-control',
+                            'tooltip' => 'custom.object.help.name.singular',
+                        ],
+                    ]
+                ],
+                [
+                    'namePlural',
+                    TextType::class,
+                    [
+                        'label'      => 'custom.object.name.plural.label',
+                        'required'   => true,
+                        'label_attr' => ['class' => 'control-label'],
+                        'attr'       => [
+                            'class'   => 'form-control',
+                            'tooltip' => 'custom.object.help.name.plural',
+                        ],
+                    ]
+                ],
+                [
+                    'description',
+                    TextareaType::class,
+                    [
+                        'label'      => 'custom.object.description.label',
+                        'required'   => false,
+                        'label_attr' => ['class' => 'control-label'],
+                        'attr'       => [
+                            'class' => 'form-control editor',
+                        ],
+                    ]
+                ],
+                [
+                    'type',
+                    ChoiceType::class,
+                    [
+                        'label'      => 'custom.object.type.label',
+                        'required'   => true,
+                        'label_attr' => ['class' => 'control-label'],
+                        'attr'       => [
+                            'class' => 'form-control',
+                        ],
+                        'disabled'   => !$isNewObject,
+                        'choices'    => [
+                            'custom.object.type.master'       => CustomObject::TYPE_MASTER,
+                            'custom.object.type.relationship' => CustomObject::TYPE_RELATIONSHIP,
+                        ],
+                    ]
+                ],
+                [
+                    'relationship',
+                    ChoiceType::class,
+                    [
+                        'label'      => 'custom.object.relationship.label',
+                        'required'   => true,
+                        'placeholder' => '',
+                        'empty_data' => null,
+                        'label_attr' => ['class' => 'control-label'],
+                        'attr'       => [
+                            'class'        => 'form-control',
+                            'data-show-on' => '{"custom_object_type":["'.CustomObject::TYPE_RELATIONSHIP.'"]}',
+                        ],
+                        'choices'    => [
+                            'custom.object.relationship.many_to_many' => CustomObject::RELATIONSHIP_MANY_TO_MANY,
+                            'custom.object.relationship.one_to_one'   => CustomObject::RELATIONSHIP_ONE_TO_ONE,
+                        ],
+                        'disabled'   => !$isNewObject,
+                    ]
+                ],
+                [
+                    'masterObject',
+                    EntityType::class,
+                    [
+                        'label'         => 'custom.object.relationship.master_object.label',
+                        'class'         => CustomObject::class,
+                        'required'      => true,
+                        'placeholder'   => '',
+                        'empty_data'    => null,
+                        'label_attr'    => ['class' => 'control-label'],
+                        'attr'          => [
+                            'class'        => 'form-control',
+                            'data-show-on' => '{"custom_object_type":["'.CustomObject::TYPE_RELATIONSHIP.'"]}',
+                        ],
+                        'choice_label'  => function ($customObject) {
+                            return $customObject->getNameSingular()." (".$customObject->getAlias().")";
+                        },
+                        'choice_value'  => 'id',
+                        'query_builder' => $queryBuilder,
+                        'disabled'      => !$isNewObject,
+                    ]
+                ],
+                [
+                    'category',
+                    CategoryListType::class, ['bundle' => 'global'],
+                ],
+                [
+                    'isPublished',
+                    YesNoButtonGroupType::class,
+                ],
+                [
+                    'customFields',
+                    CollectionType::class,
+                    [
+                        'entry_type'         => CustomFieldType::class,
+                        'entry_options'      => ['custom_object_form' => true],
+                        'allow_extra_fields' => true,
+                        'allow_add'          => true,
+                        'allow_delete'       => true,
+                        'by_reference'       => false,
+                        'prototype'          => false, // Do not use CF panel prototype in DOM
+                    ]
+                ],
+                [
+                    'buttons',
+                    FormButtonsType::class,
+                    [
+                        'cancel_onclick' => "mQuery('form[name=custom_object]').attr('method', 'get').attr('action', mQuery('form[name=custom_object]').attr('action').replace('/save', '/cancel'));",
+                    ]
+                ]
+            );
+
+        $builder->expects($this->once())
+            ->method('get')
+            ->with('masterObject')
+            ->willReturn($builder);
+        $builder->expects($this->once())
+            ->method('addModelTransformer');
+        $builder->expects($this->once())
+            ->method('setAction')
+            ->with($action);
+
+        $this->type->buildForm($builder, $options);
     }
 }
