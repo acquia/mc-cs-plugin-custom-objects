7f9dc000ef7bbb96a43357bcab0b04eb4aa64063
Lukas Drahy <lukas@drahy.net>
2020-06-17 13:10:34 +0200

    Add subselect covering direct relationship between items

--- Config/config.php
+++ Config/config.php
@@ -1101,7 +1101,10 @@ return [
             ],
             'custom_object.query.filter.helper'            => [
                 'class'     => \MauticPlugin\CustomObjectsBundle\Helper\QueryFilterHelper::class,
-                'arguments' => ['custom_field.type.provider'],
+                'arguments' => [
+                    'custom_field.type.provider',
+                    'doctrine.orm.entity_manager',
+                ],
             ],
             'custom_object.helper.token_formatter' => [
                 'class'     => \MauticPlugin\CustomObjectsBundle\Helper\TokenFormatter::class,
