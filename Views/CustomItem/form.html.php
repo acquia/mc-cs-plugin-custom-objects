359304e33036c2e0a0f88b7babd9b154be8cfdc4
John Linhart <admin@escope.cz>
2020-07-17 10:28:00 +0200

    Graphically highlight which fields belong to the parent object and which to the child object

--- Views/CustomItem/form.html.php
+++ Views/CustomItem/form.html.php
@@ -45,12 +45,29 @@ $hideCategories = CustomObject::TYPE_RELATIONSHIP === $customObject->getType() ?
     <div class="col-md-9 bg-auto height-auto bdr-r">
         <div class="pa-md">
             <div class="row">
-                <div class="col-md-4">
+                <div class="col-md-6">
                     <?php echo $view['form']->rowIfExists($form, 'name'); ?>
                     <?php echo $view['form']->row($form['custom_field_values']); ?>
                     <?php echo $view['form']->rowIfExists($form, 'child_custom_field_values'); ?>
                     <?php echo $view['form']->row($form['contact_id']); ?>
                 </div>
+                <?php if ($customObject->getRelationshipObject() && !empty($form['contact_id']->vars['value'])) : ?>
+                <div class="col-md-6">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">
+                                <?php echo $view['translator']->trans(
+                                    'custom.item.new',
+                                    ['%object%' => $view['translator']->trans($customObject->getRelationshipObject()->getNameSingular())]
+                                ); ?>
+                            </h3>
+                        </div>
+                        <div class="panel-body">
+                            <?php echo $view['form']->rowIfExists($form, 'child_custom_field_values'); ?>
+                        </div>
+                    </div>
+                </div>
+                <?php endif; ?>
             </div>
         </div>
     </div>
