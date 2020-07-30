9114d7daa6bb6be61097a59f82048893dddb39ac
Lukas Drahy <lukas@drahy.net>
2020-07-30 11:51:36 +0200

    Add numeric suffix to alias of joined table level

--- Helper/CustomFieldQueryBuilder.php
+++ Helper/CustomFieldQueryBuilder.php
@@ -113,14 +113,14 @@ class CustomFieldQueryBuilder
                 ->innerJoin(
                     "{$alias}_value",
                     MAUTIC_TABLE_PREFIX.'custom_item_xref_custom_item',
-                    "{$alias}_item_xref",
-                    "{$alias}_item_xref.custom_item_id_lower = {$alias}_value.custom_item_id"
+                    "{$alias}_item_xref_1",
+                    "{$alias}_item_xref_1.custom_item_id_lower = {$alias}_value.custom_item_id"
                 )
                 ->innerJoin(
                     "{$alias}_value",
                     MAUTIC_TABLE_PREFIX.'custom_item_xref_contact',
                     "{$alias}_contact",
-                    "{$alias}_item_xref.custom_item_id_higher = {$alias}_contact.custom_item_id"
+                    "{$alias}_item_xref_1.custom_item_id_higher = {$alias}_contact.custom_item_id"
                 )
                 ->andWhere(
                     $qb->expr()->eq("{$alias}_value.custom_field_id", ":{$alias}_custom_field_id")
@@ -136,14 +136,14 @@ class CustomFieldQueryBuilder
                 ->innerJoin(
                     "{$alias}_value",
                     MAUTIC_TABLE_PREFIX.'custom_item_xref_custom_item',
-                    "{$alias}_item_xref",
-                    "{$alias}_item_xref.custom_item_id_higher = {$alias}_value.custom_item_id"
+                    "{$alias}_item_xref_1",
+                    "{$alias}_item_xref_1.custom_item_id_higher = {$alias}_value.custom_item_id"
                 )
                 ->innerJoin(
                     "{$alias}_value",
                     MAUTIC_TABLE_PREFIX.'custom_item_xref_contact',
                     "{$alias}_contact",
-                    "{$alias}_item_xref.custom_item_id_lower = {$alias}_contact.custom_item_id"
+                    "{$alias}_item_xref_1.custom_item_id_lower = {$alias}_contact.custom_item_id"
                 )
                 ->andWhere(
                     $qb->expr()->eq("{$alias}_value.custom_field_id", ":{$alias}_custom_field_id")
