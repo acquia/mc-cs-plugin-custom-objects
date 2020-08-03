73e96302f7e78fcca3d17fda190f53e9e573984c
Lukas Drahy <lukas@drahy.net>
2020-08-03 14:52:14 +0200

    Fix DI

--- Config/config.php
+++ Config/config.php
@@ -1098,7 +1098,7 @@ return [
                     'custom_object.query.filter.helper',
                 ],
             ],
-            'custom_field_query_builder_counter' => [
+            'custom_field_query_builder_calculator' => [
                 'class'     => \MauticPlugin\CustomObjectsBundle\Helper\CustomFieldQueryBuilder\Calculator::class,
             ],
             'custom_field_query_builder' => [
@@ -1108,7 +1108,7 @@ return [
                     'custom_field.type.provider',
                     'mautic.helper.core_parameters',
                     'custom_field.repository',
-                    'custom_field_query_builder_counter',
+                    'custom_field_query_builder_calculator',
                 ],
             ],
             'custom_object.query.filter.helper'            => [
