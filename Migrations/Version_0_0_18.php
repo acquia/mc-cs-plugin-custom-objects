a239329a538e3a515f8795557f58141aa11e673b
Anton Vlasenko <vlasenko.anton@gmail.com>
2020-10-19 19:45:08 +0200

    Rearrange the code.

--- Migrations/Version_0_0_18.php
+++ Migrations/Version_0_0_18.php
@@ -28,6 +28,10 @@ class Version_0_0_18 extends AbstractMigration
      */
     protected function up(): void
     {
+        if ($this->schema->getTable($this->concatPrefix('custom_object'))->hasForeignKey('FK_9C007FE8594D0CC2')) {
+            $this->addSql("ALTER TABLE {$this->concatPrefix('custom_object')} DROP FOREIGN KEY FK_9C007FE8594D0CC2;");
+        }
+
         // Dropping that index because we use UNIQ_CO_MASTER_OBJECT index instead.
         if ($this->schema->getTable($this->concatPrefix('custom_object'))->hasIndex('master_object')) {
             $this->addSql("ALTER TABLE {$this->concatPrefix('custom_object')} DROP INDEX master_object;");
@@ -35,10 +39,6 @@ class Version_0_0_18 extends AbstractMigration
 
         // Fix column type. Sometimes it doesn't match what we have in the Version_0_0_11.php. Version_0_0_11 is not correct.
         $this->addSql("ALTER TABLE {$this->concatPrefix('custom_object')} CHANGE COLUMN `master_object` `master_object` INT(10) UNSIGNED NULL DEFAULT NULL;");
-
-        if ($this->schema->getTable($this->concatPrefix('custom_object'))->hasForeignKey('FK_9C007FE8594D0CC2')) {
-            $this->addSql("ALTER TABLE {$this->concatPrefix('custom_object')} DROP FOREIGN KEY FK_9C007FE8594D0CC2;");
-        }
 
         if ($this->schema->getTable($this->concatPrefix('custom_object'))->hasIndex('UNIQ_9C007FE8594D0CC2')) {
             $this->addSql("ALTER TABLE {$this->concatPrefix('custom_object')} DROP INDEX UNIQ_9C007FE8594D0CC2;");
