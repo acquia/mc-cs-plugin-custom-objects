50a1e1c56ea4e3d9446deb85b0ea6dbad21bcebc
John Linhart <admin@escope.cz>
2022-02-17 13:59:09 +0100

    Removing unused private property and the json file it was using

--- Helper/RandomHelper.php
+++ Helper/RandomHelper.php
@@ -51,27 +51,4 @@ class RandomHelper
     {
         return uniqid('', true) . '@' . uniqid('', true) . '.net';
     }
-
-    /**
-     * Loads the database of random words from a JSON file or cache if loaded already.
-     *
-     * @return string[]
-     *
-     * @throws \Exception
-     */
-    private function getRandomWords(): array
-    {
-        if (empty($this->randomWords)) {
-            $path     = __DIR__.'/../Assets/json/mnemonic-words.json';
-            $contents = \file_get_contents($path);
-
-            if (false === $contents) {
-                throw new \Exception("Could not fetch file contents from {$path}");
-            }
-
-            $this->randomWords = \json_decode($contents, true);
-        }
-
-        return $this->randomWords;
-    }
 }
