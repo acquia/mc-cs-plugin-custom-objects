612ae472b288cf401fb404e301e03f7f1e049cb3
aarohiprasad <aarohi.prasad@acquia.com>
2022-09-23 16:43:30 +0530

    corrections

--- Entity/CustomObject.php
+++ Entity/CustomObject.php
@@ -420,6 +420,14 @@ class CustomObject extends FormEntity implements UniqueEntityInterface
         );
     }
 
+    public function getFieldsIsUniqueIdentifier(): ?ArrayCollection
+    {
+        return $this->customFields->filter(
+            static fn(CustomField $customField) => $customField->getIsUniqueIdentifier()
+        );
+    }
+
+
     /**
      * Called when the custom fields are loaded from the database.
      */
