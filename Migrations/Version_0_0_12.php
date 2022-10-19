08c4c0244dafcf3193bcd29ffafd3e783428aac8
Rohit Pavaskar <rohit.pavaskar@acquia.com>
2022-10-19 18:17:52 +0530

    Fix migration

--- Migrations/Version_0_0_12.php
+++ Migrations/Version_0_0_12.php
@@ -10,11 +10,14 @@ use Mautic\IntegrationsBundle\Migration\AbstractMigration;
 
 class Version_0_0_12 extends AbstractMigration
 {
+    /** @var Schema */
+    private $schema;
     /**
      * {@inheritdoc}
      */
     protected function isApplicable(Schema $schema): bool
     {
+        $this->schema = $schema;
         try {
             return !$schema->getTable($this->concatPrefix('custom_item'))->hasIndex('name_fulltext');
         } catch (SchemaException $e) {
@@ -28,9 +31,17 @@ class Version_0_0_12 extends AbstractMigration
     protected function up(): void
     {
         $this->addSql("CREATE FULLTEXT INDEX name_fulltext ON {$this->concatPrefix('custom_item')} (name)");
-        $this->addSql("CREATE FULLTEXT INDEX value_fulltext ON {$this->concatPrefix('custom_field_value_text')} (value)");
-        $this->addSql("CREATE FULLTEXT INDEX value_fulltext ON {$this->concatPrefix('custom_field_value_option')} (value)");
-        $this->addSql("ALTER TABLE {$this->concatPrefix('custom_field_value_option')} ADD id BIGINT UNSIGNED AUTO_INCREMENT NOT NULL, DROP PRIMARY KEY, ADD PRIMARY KEY (id)");
-        $this->addSql("CREATE UNIQUE INDEX `unique` ON {$this->concatPrefix('custom_field_value_option')} (value, custom_field_id, custom_item_id)");
+        if (!$this->schema->getTable($this->concatPrefix('custom_field_value_text'))->hasIndex('value_fulltext')) {
+            $this->addSql("CREATE FULLTEXT INDEX value_fulltext ON {$this->concatPrefix('custom_field_value_text')} (value)");
+        }
+        if (!$this->schema->getTable($this->concatPrefix('custom_field_value_option'))->hasIndex('value_fulltext')) {
+            $this->addSql("CREATE FULLTEXT INDEX value_fulltext ON {$this->concatPrefix('custom_field_value_option')} (value)");
+        }
+        if (!$this->schema->getTable($this->concatPrefix('custom_field_value_option'))->hasColumn('id')) {
+            $this->addSql("ALTER TABLE {$this->concatPrefix('custom_field_value_option')} ADD id BIGINT UNSIGNED AUTO_INCREMENT NOT NULL, DROP PRIMARY KEY, ADD PRIMARY KEY (id)");
+        }
+        if (!$this->schema->getTable($this->concatPrefix('custom_field_value_option'))->hasIndex('unique')) {
+            $this->addSql("CREATE UNIQUE INDEX `unique` ON {$this->concatPrefix('custom_field_value_option')} (value, custom_field_id, custom_item_id)");
+        }
     }
 }
