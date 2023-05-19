8462965b57594070f20c8bc5d925fdb8ce7d2421
Volha Pivavarchyk <volha@aivie.ch>
2023-05-19 14:07:23 +0300

    remove redundant functionality

--- EventListener/FormSubscriber.php
+++ EventListener/FormSubscriber.php
@@ -6,7 +6,6 @@ namespace MauticPlugin\CustomObjectsBundle\EventListener;
 
 use Mautic\FormBundle\Crate\ObjectCrate;
 use Mautic\FormBundle\Event\FieldCollectEvent;
-use Mautic\FormBundle\Event\FieldDisplayEvent;
 use Mautic\FormBundle\Event\ObjectCollectEvent;
 use Mautic\FormBundle\FormEvents;
 use Mautic\FormBundle\Crate\FieldCrate;
@@ -14,29 +13,19 @@ use MauticPlugin\CustomObjectsBundle\Entity\CustomField;
 use MauticPlugin\CustomObjectsBundle\Exception\NotFoundException;
 use MauticPlugin\CustomObjectsBundle\Model\CustomItemModel;
 use MauticPlugin\CustomObjectsBundle\Model\CustomObjectModel;
-use MauticPlugin\CustomObjectsBundle\Provider\CustomItemRouteProvider;
-use MauticPlugin\CustomObjectsBundle\Repository\CustomItemXrefContactRepository;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\Routing\RouterInterface;
 
 class FormSubscriber implements EventSubscriberInterface
 {
     private CustomObjectModel $customObjectModel;
     private CustomItemModel $customItemModel;
-    private CustomItemXrefContactRepository $customItemXrefContactRepository;
-
-    private RouterInterface $router;
 
     public function __construct(
         CustomObjectModel $customObjectModel,
-        CustomItemModel $customItemModel,
-        CustomItemXrefContactRepository $customItemXrefContactRepository,
-        RouterInterface $router
+        CustomItemModel $customItemModel
     ) {
         $this->customObjectModel               = $customObjectModel;
         $this->customItemModel                 = $customItemModel;
-        $this->customItemXrefContactRepository = $customItemXrefContactRepository;
-        $this->router                          = $router;
     }
 
     public static function getSubscribedEvents(): array
@@ -44,7 +33,6 @@ class FormSubscriber implements EventSubscriberInterface
         return [
             FormEvents::ON_OBJECT_COLLECT      => ['onObjectCollect', 0],
             FormEvents::ON_FIELD_COLLECT       => ['onFieldCollect', 0],
-            FormEvents::ON_FIELD_DISPLAY_EVENT => ['onFieldDisplay', 0],
         ];
     }
 
@@ -66,21 +54,6 @@ class FormSubscriber implements EventSubscriberInterface
 
         $items = $this->customItemModel->fetchCustomItemsForObject($object);
 
-        if ($event->isAssigned()) {
-            $contactId = $event->getLead()?->getId();
-
-            $items = array_filter(
-                $items,
-                function ($item) use ($contactId) {
-                    $ids = $this->customItemXrefContactRepository->getContactIdsLinkedToCustomItem((int)$item->getId(), 200, 0);
-                    $ids = array_column($ids, 'contact_id');
-                    return in_array($contactId, $ids);
-                }
-            );
-
-            $event->removeFields();
-        }
-
         if (count($items) > 0) {
             foreach ($items as $item) {
                 $list[$item->getId()] = $item->getName();
@@ -93,32 +66,6 @@ class FormSubscriber implements EventSubscriberInterface
             $list = $this->getCustomFieldValues($field, $items);
             $event->appendField(new FieldCrate($field->getAlias(), $field->getName(), $field->getType(), ['list' => $list]));
         }
-    }
-
-    public function onFieldDisplay(FieldDisplayEvent $event): void
-    {
-        try {
-            $object = $this->customObjectModel->fetchEntityByAlias($event->getObject());
-        } catch (NotFoundException $e) {
-            // Do nothing if the custom object doesn't exist.
-            return;
-        }
-
-        $ids   = explode(',', $event->getValue());
-        $value = '';
-
-        foreach ($ids as $id) {
-            $item = $this->customItemModel->getEntity($id);
-            if ($item) {
-                $viewParameters = [
-                    'objectId' => $object->getId(),
-                    'itemId' => $item->getId(),
-                ];
-                $route = $this->router->generate(CustomItemRouteProvider::ROUTE_VIEW, $viewParameters);
-                $value .= '<a href="' . $route . '" class="label label-success mr-5"> '.$item->getId().'</a>';
-            }
-        }
-        $event->setValue($value);
     }
 
     /**
