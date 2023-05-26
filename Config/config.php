790a9adc7c762707280880e01b528513d22d46da
Zdeno Kuzmany <zdeno@kuzmany.biz>
2023-05-26 08:35:56 +0200

    Switch description for custom object to longer type

--- Config/config.php
+++ Config/config.php
@@ -10,7 +10,7 @@ use MauticPlugin\CustomObjectsBundle\Provider\CustomObjectRouteProvider;
 $coParams = [
     'name'        => 'Custom Objects',
     'description' => 'Adds custom objects and fields features to Mautic',
-    'version'     => '0.0.27',
+    'version'     => '0.0.28',
     'author'      => 'Mautic, Inc.',
 
     'routes' => [
