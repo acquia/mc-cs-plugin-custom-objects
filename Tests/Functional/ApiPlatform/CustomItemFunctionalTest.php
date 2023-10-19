88e93cc4ada1e94c7e561e03d36373f2ba0d1075
Robert Parker <robert.parker@acquia.com>
2023-10-18 17:54:53 -0700

    simplify create test

--- Tests/Functional/ApiPlatform/CustomItemFunctionalTest.php
+++ Tests/Functional/ApiPlatform/CustomItemFunctionalTest.php
@@ -22,49 +22,19 @@ final class CustomItemFunctionalTest extends AbstractApiPlatformFunctionalTest
         parent::setUp();
     }
 
-    /**
-     * @param array<string> $permissions
-     *
-     * @dataProvider getCreateCustomItemProvider
-     */
-    public function testCreateCustomItem(array $permissions, int $httpCreated): void
+    public function testCreateCustomItem(): void
     {
         $customObject = $this->createCustomObject();
         $category     = $this->createCategory();
         $customField  = $this->createCustomField($customObject);
         $user         = $this->getUser();
 
-        $this->setPermission($user, 'custom_objects:'.$customObject->getId(), $permissions);
+        $this->setPermission($user, 'custom_objects:'.$customObject->getId(), ['create']);
 
         $payloadCreate        = $this->getCreatePayload($customObject, $category, $customField);
         $clientCreateResponse = $this->createEntity('custom_items', $payloadCreate);
 
-        $this->assertEquals($httpCreated, $clientCreateResponse->getStatusCode());
-    }
-
-    /**
-     * @return array[array<array<string>|int>]
-     */
-    public function getCreateCustomItemProvider(): array
-    {
-        return [
-            [
-                ['viewown', 'viewother', 'editown', 'editother', 'create', 'deleteown', 'deleteother', 'publishown', 'publishother'],
-                Response::HTTP_CREATED,
-            ],
-            [
-                ['viewown', 'viewother', 'editown', 'editother', 'create', 'publishown', 'publishother'],
-                Response::HTTP_CREATED,
-            ],
-            [
-                ['viewown', 'viewother', 'create', 'deleteown', 'deleteother', 'publishown', 'publishother'],
-                Response::HTTP_CREATED,
-            ],
-            [
-                ['viewown', 'viewother', 'editown', 'editother', 'deleteown', 'deleteother', 'publishown', 'publishother'],
-                Response::HTTP_FORBIDDEN,
-            ],
-        ];
+        $this->assertEquals(Response::HTTP_CREATED, $clientCreateResponse->getStatusCode());
     }
 
     public function testRetrieveCustomItem(): void
