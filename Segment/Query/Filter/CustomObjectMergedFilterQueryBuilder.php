62ae4c4d02eaa1e679b2b99d947350b0db2f5a08
Tejas Navghane <ts.navghane@gmail.com>
2024-02-13 02:01:48 +0530

    Add CustomObjectMergedFilterQueryBuilder

---
+++ Segment/Query/Filter/CustomObjectMergedFilterQueryBuilder.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MauticPlugin\CustomObjectsBundle\Segment\Query\Filter;
+
+use Mautic\LeadBundle\Segment\ContactSegmentFilter;
+use Mautic\LeadBundle\Segment\Query\Filter\BaseFilterQueryBuilder;
+use Mautic\LeadBundle\Segment\Query\QueryBuilder;
+use Mautic\LeadBundle\Segment\RandomParameterName;
+use MauticPlugin\CustomObjectsBundle\Helper\QueryFilterHelper;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+class CustomObjectMergedFilterQueryBuilder extends BaseFilterQueryBuilder
+{
+    private QueryFilterHelper $queryFilterHelper;
+
+    public function __construct(
+        RandomParameterName $randomParameterNameService,
+        EventDispatcherInterface $dispatcher,
+        QueryFilterHelper $queryFilterHelper
+    ) {
+        parent::__construct($randomParameterNameService, $dispatcher);
+        $this->queryFilterHelper = $queryFilterHelper;
+    }
+
+    public static function getServiceId(): string
+    {
+        return 'mautic.lead.query.builder.custom_object.merged.value';
+    }
+
+    public function applyQuery(QueryBuilder $queryBuilder, ContactSegmentFilter $filter): QueryBuilder
+    {
+        $leadsTableAlias = $queryBuilder->getTableAlias(MAUTIC_TABLE_PREFIX.'leads');
+        $filterOperator  = $filter->getOperator();
+        $subQuery        = $this->queryFilterHelper->createMergeFilterQuery($filter, $leadsTableAlias);
+
+        switch ($filterOperator) {
+            case 'empty':
+            case 'neq':
+            case 'notLike':
+            case '!multiselect':
+                $queryBuilder->addLogic(
+                    $queryBuilder->expr()->notExists($subQuery->getSQL()),
+                    $filter->getGlue()
+                );
+
+                break;
+            default:
+                $queryBuilder->addLogic(
+                    $queryBuilder->expr()->exists($subQuery->getSQL()),
+                    $filter->getGlue()
+                );
+        }
+
+        $queryBuilder->setParameters($subQuery->getParameters(), $subQuery->getParameterTypes());
+
+        return $queryBuilder;
+    }
+}
