9b8582c07c394751acd38067e3292ead6aebf219
fedys <miroslav.fedeles@gmail.com>
2025-05-21 13:16:32 +0200

    Removed duplicity with MatchFilterForLeadTrait::matchFilterForLead() Added polyfill for MatchFilterForLeadTrait

--- Helper/ContactFilterMatcher.php
+++ Helper/ContactFilterMatcher.php
@@ -6,8 +6,6 @@ namespace MauticPlugin\CustomObjectsBundle\Helper;
 
 use Mautic\EmailBundle\EventListener\MatchFilterForLeadTrait;
 use Mautic\LeadBundle\Entity\LeadListRepository;
-use Mautic\LeadBundle\Exception\OperatorsNotFoundException;
-use Mautic\LeadBundle\Segment\OperatorOptions;
 use MauticPlugin\CustomObjectsBundle\DTO\TableConfig;
 use MauticPlugin\CustomObjectsBundle\Entity\CustomItem;
 use MauticPlugin\CustomObjectsBundle\Entity\CustomObject;
@@ -16,10 +14,19 @@ use MauticPlugin\CustomObjectsBundle\Exception\NotFoundException;
 use MauticPlugin\CustomObjectsBundle\Model\CustomFieldModel;
 use MauticPlugin\CustomObjectsBundle\Model\CustomItemModel;
 use MauticPlugin\CustomObjectsBundle\Model\CustomObjectModel;
+use MauticPlugin\CustomObjectsBundle\Polyfill\EventListener\MatchFilterForLeadTrait as MatchFilterForLeadTraitPolyfill;
+
+if (method_exists(MatchFilterForLeadTrait::class, 'transformFilterDataForLead')) {
+    class_alias(MatchFilterForLeadTrait::class, '\MauticPlugin\CustomObjectsBundle\Helper\MatchFilterForLeadTraitAlias');
+} else {
+    class_alias(MatchFilterForLeadTraitPolyfill::class, '\MauticPlugin\CustomObjectsBundle\Helper\MatchFilterForLeadTraitAlias');
+}
 
 class ContactFilterMatcher
 {
-    use MatchFilterForLeadTrait;
+    use MatchFilterForLeadTraitAlias {
+        transformFilterDataForLead as transformFilterDataForLeadAlias;
+    }
 
     private CustomFieldModel $customFieldModel;
     private CustomObjectModel $customObjectModel;
@@ -55,7 +62,7 @@ class ContactFilterMatcher
         $hasCustomFields = true;
         $lead            = array_merge($lead, $customFieldValues);
 
-        return $this->matchFilterForLeadInCustomObject($filters, $lead);
+        return $this->matchFilterForLead($filters, $lead);
     }
 
     /**
@@ -166,189 +173,17 @@ class ContactFilterMatcher
     }
 
     /**
-     * We have a similar function in MatchFilterForLeadTrait since we are unable to alter anything in Mautic 4.4,
-     * hence there is some duplication of code.
-     *
-     * @param mixed[] $filter
+     * @param mixed[] $data
      * @param mixed[] $lead
+     *
+     * @return ?mixed[]
      */
-    protected function matchFilterForLeadInCustomObject(array $filter, array $lead): bool
+    private function transformFilterDataForLead(array $data, array $lead): ?array
     {
-        if (empty($lead['id'])) {
-            // Lead in generated for preview with faked data
-            return false;
+        if ('custom_object' === $data['object']) {
+            return $lead[$data['field']];
         }
 
-        $groups   = [];
-        $groupNum = 0;
-
-        foreach ($filter as $data) {
-            $isCompanyField = (0 === strpos($data['field'], 'company') && 'company' !== $data['field']);
-            $primaryCompany = ($isCompanyField && !empty($lead['companies'])) ? $lead['companies'][0] : null;
-
-            /*
-             * Split the filters into groups based on the glue.
-             * The first filter and any filters whose glue is
-             * "or" will start a new group.
-             */
-            if (0 === $groupNum || 'or' === $data['glue']) {
-                ++$groupNum;
-                $groups[$groupNum] = null;
-            }
-
-            /*
-             * If the group has been marked as false, there
-             * is no need to continue checking the others
-             * in the group.
-             */
-            if (false === $groups[$groupNum]) {
-                continue;
-            }
-
-            /*
-             * If we are checking the first filter in a group
-             * assume that the group will not match.
-             */
-            if (null === $groups[$groupNum]) {
-                $groups[$groupNum] = false;
-            }
-
-            if ('leadlist' === $data['type']) {
-                $groups[$groupNum] = $this->isContactSegmentRelationshipValid(
-                    (int) $lead['id'], $data['operator'], $data['filter']
-                );
-            }
-
-            if ($isCompanyField) {
-                if (empty($primaryCompany)) {
-                    continue;
-                }
-            } else {
-                if (!array_key_exists($data['field'], $lead)) {
-                    continue;
-                }
-            }
-
-            $leadValues   = 'custom_object' === $data['object'] ? $lead[$data['field']] : [$isCompanyField ? $primaryCompany[$data['field']] : $lead[$data['field']]];
-            $filterVal    = $data['filter'];
-            $subgroup     = null;
-
-            if (is_array($leadValues)) {
-                foreach ($leadValues as $leadVal) {
-                    if ($subgroup) {
-                        break;
-                    }
-
-                    switch ($data['type']) {
-                        case 'boolean':
-                            if (null !== $leadVal) {
-                                $leadVal = (bool) $leadVal;
-                            }
-
-                            if (null !== $filterVal) {
-                                $filterVal = (bool) $filterVal;
-                            }
-                            break;
-                        case 'datetime':
-                        case 'time':
-                            if (!is_null($leadVal) && !is_null($filterVal)) {
-                                $leadValCount   = substr_count($leadVal, ':');
-                                $filterValCount = substr_count($filterVal, ':');
-
-                                if (2 === $leadValCount && 1 === $filterValCount) {
-                                    $filterVal .= ':00';
-                                }
-                            }
-                            break;
-                        case 'tags':
-                        case 'select':
-                        case 'multiselect':
-                            if (!is_array($leadVal) && !empty($leadVal)) {
-                                $leadVal = explode('|', $leadVal);
-                            }
-                            if (!is_null($filterVal) && !is_array($filterVal)) {
-                                $filterVal = explode('|', $filterVal);
-                            }
-                            break;
-                        case 'number':
-                            $leadVal   = (int) $leadVal;
-                            $filterVal = (int) $filterVal;
-                            break;
-                    }
-
-                    switch ($data['operator']) {
-                        case '=':
-                            if ('boolean' === $data['type']) {
-                                $groups[$groupNum] = $leadVal === $filterVal;
-                            } else {
-                                $groups[$groupNum] = $leadVal == $filterVal;
-                            }
-                            break;
-                        case '!=':
-                            if ('boolean' === $data['type']) {
-                                $groups[$groupNum] = $leadVal !== $filterVal;
-                            } else {
-                                $groups[$groupNum] = $leadVal != $filterVal;
-                            }
-                            break;
-                        case 'gt':
-                            $groups[$groupNum] = $leadVal > $filterVal;
-                            break;
-                        case 'gte':
-                            $groups[$groupNum] = $leadVal >= $filterVal;
-                            break;
-                        case 'lt':
-                            $groups[$groupNum] = $leadVal < $filterVal;
-                            break;
-                        case 'lte':
-                            $groups[$groupNum] = $leadVal <= $filterVal;
-                            break;
-                        case 'empty':
-                            $groups[$groupNum] = empty($leadVal);
-                            break;
-                        case '!empty':
-                            $groups[$groupNum] = !empty($leadVal);
-                            break;
-                        case 'like':
-                            $matchVal          = str_replace(['.', '*', '%'], ['\.', '\*', '.*'], $filterVal);
-                            $groups[$groupNum] = 1 === preg_match('/'.$matchVal.'/', $leadVal);
-                            break;
-                        case '!like':
-                            $matchVal          = str_replace(['.', '*'], ['\.', '\*'], $filterVal);
-                            $matchVal          = str_replace('%', '.*', $matchVal);
-                            $groups[$groupNum] = 1 !== preg_match('/'.$matchVal.'/', $leadVal);
-                            break;
-                        case OperatorOptions::IN:
-                            $groups[$groupNum] = $this->checkLeadValueIsInFilter($leadVal, $filterVal, false);
-                            break;
-                        case OperatorOptions::NOT_IN:
-                            $groups[$groupNum] = $this->checkLeadValueIsInFilter($leadVal, $filterVal, true);
-                            break;
-                        case 'regexp':
-                            $groups[$groupNum] = 1 === preg_match('/'.$filterVal.'/i', $leadVal);
-                            break;
-                        case '!regexp':
-                            $groups[$groupNum] = 1 !== preg_match('/'.$filterVal.'/i', $leadVal);
-                            break;
-                        case 'startsWith':
-                            $groups[$groupNum] = 0 === strncmp($leadVal, $filterVal, strlen($filterVal));
-                            break;
-                        case 'endsWith':
-                            $endOfString       = substr($leadVal, strlen($leadVal) - strlen($filterVal));
-                            $groups[$groupNum] = 0 === strcmp($endOfString, $filterVal);
-                            break;
-                        case 'contains':
-                            $groups[$groupNum] = false !== strpos((string) $leadVal, (string) $filterVal);
-                            break;
-                        default:
-                            throw new OperatorsNotFoundException('Operator is not defined or invalid operator found.');
-                    }
-
-                    $subgroup = $groups[$groupNum];
-                }
-            }
-        }
-
-        return in_array(true, $groups);
+        return $this->transformFilterDataForLeadAlias($data, $lead);
     }
 }
